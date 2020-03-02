313da6c9dbd0c9c56e193122481ff9b5c90a9a09
minazhou2020 <61678018+minazhou2020@users.noreply.github.com>
2020-03-02 09:14:49 -0600

    Update CrackSeedDetection.h

--- CrackDetection/CrackSeedDetection.h
+++ CrackDetection/CrackSeedDetection.h
@@ -19,11 +19,11 @@ using namespace std;
 class CrackSeedDetection
 {
 public:
-	static vector<Point2f> convert_to_seed(int height_count, int width_count, vector<Mat> cells, float threshold);
+	static vector<Point2f> convert_to_seed(const int height_count, const int width_count, vector<Mat>& cells, float threshold);
 	static void find_nearest_neighbor(vector<Point2f>& seeds, Graph& g);
 private:
-	static int neighbor_mean_total(int x_coors[], int y_coors[], vector<Mat> cells, int i, int j, int height_count);
-	static float cell_constrast(int avg, int center, int guide);
-	static int is_seed(vector<Mat> cells, int center_mean, int i, int j, int height_count, float threshold);
+	static int neighbor_mean_total(const int x_coors[], const int y_coors[], const vector<Mat> cells, const int i, const int j, const int height_count);
+	static float cell_constrast(const int avg, const int center, const int guide);
+	static int is_seed(const vector<Mat> cells, const int center_mean, const int i, const int j, const int height_count);
 
 };
