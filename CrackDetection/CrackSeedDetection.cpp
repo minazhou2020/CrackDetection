79915b5f8ca1741fb84a55c95bd9c077a513cd13
minazhou2020 <61678018+minazhou2020@users.noreply.github.com>
2020-03-02 09:12:40 -0600

    Update CrackSeedDetection.cpp

--- CrackDetection/CrackSeedDetection.cpp
+++ CrackDetection/CrackSeedDetection.cpp
@@ -6,20 +6,20 @@ A preprocessed image was divided into grid cells of 8×8 pixels, and the cells w
 crack seed by analyzing brightness changes on neighboring cells. */
 
 //coordinates of four neighbor cells in the eight proposed pattern 
-int trans_n_x[4] = { 0, 0, 0, 0 };
-int trans_n_y[4] = { -2, -1, 1, 2 };
-int longi_n_x[4] = { -2, -1, 1, 2 };
-int longi_n_y[4] = { 0, 0, 0, 0 };
-int backward_x[4] = { -2, -1, 1, 2 };
-int backward_y[4] = { -2, -1, 1, 2 };
-int forward_x[4] = { -2, -1, 1, 2 };
-int forward_y[4] = { 2, 1, -1, -2 };
+const int trans_n_x[4] = { 0, 0, 0, 0 };
+const int trans_n_y[4] = { -2, -1, 1, 2 };
+const int longi_n_x[4] = { -2, -1, 1, 2 };
+const int longi_n_y[4] = { 0, 0, 0, 0 };
+const int backward_x[4] = { -2, -1, 1, 2 };
+const int backward_y[4] = { -2, -1, 1, 2 };
+const int forward_x[4] = { -2, -1, 1, 2 };
+const int forward_y[4] = { 2, 1, -1, -2 };
 
 // caculate the average intensity of four neighbor cells of each seed
-int CrackSeedDetection::neighbor_mean_total(int x_coors[], int y_coors[], vector<Mat> cells, int i, int j, int height_count) {
+int CrackSeedDetection::neighbor_mean_total(const int x_coors[], const int y_coors[], const vector<Mat> cells, const int i, const int j, const int width_count) {
 	int mean_neighbor_total = 0;
 	for (int m = 0; m < NEIGHBOR_NUMBER; m++) {
-		Mat n = cells[(i + x_coors[m])*height_count + j+ y_coors[m]];
+		Mat n = cells[(i + x_coors[m])*width_count + j+ y_coors[m]];
 		Scalar mean = cv::mean(n);
 		mean_neighbor_total += mean[0];
 	}
@@ -31,34 +31,8 @@ float CrackSeedDetection::cell_constrast(int avg, int center, int guide) {
 	return (float)(2 * avg - center - guide) / (float)avg;
 }
 
-// verify if a cell is a seed cell
-int CrackSeedDetection::is_seed(vector<Mat> cells, int center_mean, int i, int j, int width_count, float threshold) {
-	float max_contrast = 0;
-	int g1 = mean(cells[(i - 1)*width_count + j])[0];
-	int g2 = mean(cells[(i + 1)*width_count + j])[0];
-	int trans_mean_total = neighbor_mean_total(trans_n_x, trans_n_y, cells, i, j, width_count);
-	max_contrast = max(cell_constrast(trans_mean_total, center_mean, g1), max_contrast);
-	max_contrast = max(cell_constrast(trans_mean_total, center_mean, g2),max_contrast);
-	int g3 = mean(cells[i*width_count + j + 1])[0];
-	int g4 = mean(cells[i*width_count + j - 1])[0];
-	int longi_mean_total = neighbor_mean_total(longi_n_x, longi_n_y, cells, i, j, width_count);
-	max_contrast = max(cell_constrast(longi_mean_total, center_mean, g3),max_contrast);
-	max_contrast = max(cell_constrast(longi_mean_total, center_mean, g4), max_contrast);
-	int g5 = mean(cells[(i - 1)*width_count + j + 1])[0];
-	int g6 = mean(cells[(i + 1)*width_count + j - 1])[0];
-	int back_mean_total = neighbor_mean_total(backward_x, backward_y, cells, i, j, width_count);
-	max_contrast = max(cell_constrast(back_mean_total, center_mean, g5), max_contrast);
-	max_contrast = max(cell_constrast(back_mean_total, center_mean, g6), max_contrast);
-	int g7 = mean(cells[(i - 1)*width_count + j - 1])[0];
-	int g8 = mean(cells[(i + 1)*width_count + j + 1])[0];
-	int forward_mean_total = neighbor_mean_total(forward_x, forward_y, cells, i, j, width_count);
-	max_contrast = max(cell_constrast(back_mean_total, center_mean, g7), max_contrast);
-	max_contrast=max(cell_constrast(back_mean_total, center_mean, g8), max_contrast);
-	return max_contrast > threshold;
-}
-
 // create a vector of seeds to maintain the verified seed cells
-vector<Point2f> CrackSeedDetection::convert_to_seed(int height_count, int width_count, vector<Mat> cells, float threshold)
+vector<Point2f> CrackSeedDetection::convert_to_seed(const int height_count, const int width_count, vector<Mat>& cells)
 {
 	vector<Point2f> seeds = vector<Point2f>();
 	for (int i = NEIGHBOR_NUMBER /2; i < height_count - NEIGHBOR_NUMBER/2; i++) {
@@ -113,5 +87,31 @@ void CrackSeedDetection::find_nearest_neighbor(vector<Point2f>& seeds, Graph& g)
 		}
 	}
 
+}
+
+// verify if a cell is a seed cell
+int CrackSeedDetection::is_seed(const vector<Mat> cells, const int center_mean, const int i, const int j, const int width_count, const float threshold) {
+	float max_contrast = 0;
+	int g1 = mean(cells[(i - 1)*width_count + j])[0];
+	int g2 = mean(cells[(i + 1)*width_count + j])[0];
+	int trans_mean_total = neighbor_mean_total(trans_n_x, trans_n_y, cells, i, j, width_count);
+	max_contrast = max(cell_constrast(trans_mean_total, center_mean, g1), max_contrast);
+	max_contrast = max(cell_constrast(trans_mean_total, center_mean, g2),max_contrast);
+	int g3 = mean(cells[i*width_count + j + 1])[0];
+	int g4 = mean(cells[i*width_count + j - 1])[0];
+	int longi_mean_total = neighbor_mean_total(longi_n_x, longi_n_y, cells, i, j, width_count);
+	max_contrast = max(cell_constrast(longi_mean_total, center_mean, g3),max_contrast);
+	max_contrast = max(cell_constrast(longi_mean_total, center_mean, g4), max_contrast);
+	int g5 = mean(cells[(i - 1)*width_count + j + 1])[0];
+	int g6 = mean(cells[(i + 1)*width_count + j - 1])[0];
+	int back_mean_total = neighbor_mean_total(backward_x, backward_y, cells, i, j, width_count);
+	max_contrast = max(cell_constrast(back_mean_total, center_mean, g5), max_contrast);
+	max_contrast = max(cell_constrast(back_mean_total, center_mean, g6), max_contrast);
+	int g7 = mean(cells[(i - 1)*width_count + j - 1])[0];
+	int g8 = mean(cells[(i + 1)*width_count + j + 1])[0];
+	int forward_mean_total = neighbor_mean_total(forward_x, forward_y, cells, i, j, width_count);
+	max_contrast = max(cell_constrast(back_mean_total, center_mean, g7), max_contrast);
+	max_contrast=max(cell_constrast(back_mean_total, center_mean, g8), max_contrast);
+	return max_contrast > threshold;
 }
 
